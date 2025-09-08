23a2446da06fd772428b20a773261fa942242af4
c2007523 <c2007523@admiral.bios.cf.ac.uk>
2025-09-08 14:22:00 +0100

    TPM calculate ESC

--- RNA/esc/TPM.R
+++ RNA/esc/TPM.R
@@ -1,12 +1,12 @@
 #Read in feature counts (GSM was better qualiity)
 setwd ("/mnt/clusters/admiral/data/c2007523/FYP")
 # Load table
-flcfc <- read.table("/mnt/clusters/admiral/data/c2007523/FYP/RNA/flc/featureCounts/GSM661638.featurecount.txt", header = TRUE, comment.char = "#", stringsAsFactors = FALSE)
-head(flcfc)
+escfc <- read.table("/mnt/clusters/admiral/data/c2007523/FYP/RNA/esc/featureCounts/GSM723776.featurecount.txt", header = TRUE, comment.char = "#", stringsAsFactors = FALSE)
+head(escfc)
 # Extract relevant columns
-Geneid<- flcfc$Geneid
-Length <- flcfc$Length
-Counts <- flcfc$X.home.c2007523.mydata.c2007523.FYP.RNA.flc.star.GSM661638.unsort.Aligned.out.bam
+Geneid<- escfc$Geneid
+Length <- escfc$Length
+Counts <- escfc$X.home.c2007523.mydata.c2007523.FYP.RNA.esc.star.GSM723776.unsort.Aligned.out.bam
 
 # Reads Per Kilobase
 rpk <- Counts / (Length / 1000)
@@ -18,10 +18,12 @@ scalingfactor <- sum(rpk) / 1e6
 tpm <- rpk / scalingfactor
 
 # Add to dataframe
-flcfc$TPM <- tpm
-TPMflc <- cbind(flcfc$Geneid, flcfc$TPM)
+escfc$TPM <- tpm
+TPMesc <- cbind(escfc$Geneid, escfc$TPM)
 head(TPMesc)
-colnames(TPMflc) <- c("Geneid", "TPM")
-head (TPMflc)
-setwd("/mnt/clusters/admiral/data/c2007523/FYP/RNA/flc")
-write.table(TPMflc, file = "TPMflc_table.txt", sep = "\t", row.names = FALSE, quote = FALSE)
+colnames(TPMesc) <- c("Geneid", "TPM")
+head (TPMesc)
+setwd("/mnt/clusters/admiral/data/c2007523/FYP/RNA/esc")
+write.table(TPMesc, file = "TPMesc_table.txt", sep = "\t", row.names = FALSE, quote = FALSE)
+
+
